2799754cc9ba83ec98fb25c501f6be3541ad05bc
SevenHolySwords <imrath.keith@gmail.com>
2025-01-12 03:50:07 -0500

    this doesnt work properly but im going to move on

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -20,8 +20,34 @@ const CartSlice = createSlice({
             state.cartItems = state.cartItems.filter(item=> item.id !== action.payload);
         },
 
+        clearCart(state) {
+                state.cartItems=[];
+        },
+
+        increaseItemQuantity(state, action){
+            const itemToIncrease= state.cartItems.find(item=> item.id===action.payload);
+            state.itemToincrease +=1;
+        },
+
+        decreaseItemQuantity(state, action) {
+            const itemToDecrease = state.cartItems.find(item => item.id === action.payload);
+            if (itemToDecrease && itemToDecrease.quantity > 1) {
+              itemToDecrease.quantity -= 1;
+            }
+        }
+
+      
 
     }
 });
 
 
+export const {
+    addItemToCart,
+    removeItemFromCart,
+    clearCart,
+    increaseItemQuantity,
+    decreaseItemQuantity,
+
+} = CartSlice.actions;
+export default CartSlice.reducer;
