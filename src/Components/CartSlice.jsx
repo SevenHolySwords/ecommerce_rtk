5806ed9e1819dc190d58d377f282c08688b35c64
SevenHolySwords <imrath.keith@gmail.com>
2025-01-12 01:11:01 -0500

    second commit

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,27 @@
+import {createSlice} from "@reduxjs/toolkit";
+const initialState= {
+    cartItems:[]
+}
+const CartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducer: {
+        addItemToCart(state,action){
+            const existingItem = state.cartItems.find(item=> item.id === action.payload.id)
+            if(existingItem){
+                existingItem.quantity +=1;
+            } else {
+                state.cartItems.push({...action.payload, quantity: 1});
+            }
+        },
 
-const CartSlice = ({
- 
+
+        removeItemFromCart(state, action){
+            state.cartItems = state.cartItems.filter(item=> item.id !== action.payload);
+        },
+
+
+    }
 });
 
 
